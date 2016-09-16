1bb6ab7b098ab46e44a2852f8a5710d413aafab5
yugeta.koji <geta1972@gmail.com>
2016-09-16 09:06:44 +0900

    first-commit

---
+++ index.js
@@ -0,0 +1,97 @@
+(function(){
+	var $$={};
+	
+	// init
+	$$.__construct = function(){
+		window.onload = function(){
+			$$.setInputTag();
+			$$.setSaveButton();
+			$$.setClearButton();
+			$$.setLoadData();
+		};
+		
+	};
+	
+	// set-input
+	$$.setInputTag = function(){
+		var mandara = document.getElementsByClassName("mandara");
+		if(!mandara.length){return}
+		var tds = mandara[0].getElementsByTagName("td");
+		for(var i=0; i<tds.length; i++){
+			var input = document.createElement("input");
+			input.className = "inp";
+			input.placeholder = (i+1);
+			tds[i].appendChild(input);
+		}
+	};
+	// set-save
+	$$.setSaveButton = function(){
+		var saves = document.getElementsByClassName("save");
+		if(!saves.length){return}
+		for(var i=0; i<saves.length;i++){
+			saves[i].onclick = function(){
+				$$.setDataSave();
+			};
+		}
+	};
+	// set-clear
+	$$.setClearButton = function(){
+		var btns = document.getElementsByClassName("cls");
+		if(!btns.length){return}
+		for(var i=0; i<btns.length;i++){
+			btns[i].onclick = function(){
+				$$.setDataClear();
+			};
+		}
+	};
+	
+	
+	// data-save
+	$$.setDataSave = function(){
+		var mandara = document.getElementsByClassName("mandara");
+		if(!mandara.length){return}
+		var cells = mandara[0].getElementsByTagName("td");
+		if(!cells.length){return}
+		for(var i=0; i<cells.length; i++){
+			var input = cells[i].getElementsByTagName("input");
+			if(!input.length){continue}
+			var name = "mandara_"+i;
+			localStorage.setItem(name , input[0].value);
+		}
+	};
+	
+	$$.setDataClear = function(){
+		if(!confirm("All data clear?")){return}
+		
+		var mandara = document.getElementsByClassName("mandara");
+		if(!mandara.length){return}
+		var cells = mandara[0].getElementsByTagName("td");
+		if(!cells.length){return}
+		for(var i=0; i<cells.length; i++){
+			var input = cells[i].getElementsByTagName("input");
+			if(!input.length){continue}
+			var name = "mandara_"+i;
+			localStorage.removeItem(name);
+			input[0].value = "";
+		}
+	};
+	
+	// load
+	$$.setLoadData = function(){
+		var mandara = document.getElementsByClassName("mandara");
+		if(!mandara.length){return}
+		var cells = mandara[0].getElementsByTagName("td");
+		if(!cells.length){return}
+		for(var i=0; i<cells.length; i++){
+			var input = cells[i].getElementsByTagName("input");
+			if(!input.length){continue}
+			var name = "mandara_"+i;
+			var str = localStorage.getItem(name);
+			input[0].value = str;
+		}
+	};
+	
+	
+	$$.__construct();
+	return $$;
+})();
